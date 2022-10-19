b9ee437e69f5c9e322975884b01638480f0fd15f
fatihmaras <fatihmmaras@gmail.com>
2022-10-20 00:44:37 +0300

    Added Cloneable Structure

---
+++ OOP-5_Interface_ICloneable/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_5_Interface_ICloneable
+{
+            public class Book : ICloneable
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+
+            public double Price { get; set; }
+
+
+            //  2 tür clone vardır
+            //1.  deep copy  --- yeni bir kitap nesnesi oluşturmak lazım 
+            //2.  shallow copy
+            public object Clone()
+            {
+                return this.MemberwiseClone();
+
+                /*
+
+              // Deep copy option 1
+
+                 public object Clone()
+            {
+                return new Book()
+              {
+                  Id = this.Id,
+                  Title = this.Title,
+                  Price = this.Price,
+              };
+            }
+
+
+             // Deep copy option 2
+
+             public object Clone()
+            {
+              var book = new Book();
+              book.Id = this.Id;
+              book.Title = this.Title;
+              book.Price = this.Price;
+              return book;
+             }
+              */
+            }
+        }
+    }
+
---
+++ OOP-5_Interface_ICloneable/Program.cs
@@ -0,0 +1,38 @@
+﻿using OOP_5_Interface_ICloneable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_5_Interface_ICloneable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var book1 = new Book()
+            {
+                Id = 1,
+                Title = "Suç ve Ceza",
+                Price = 25.5,
+            };
+
+            var book2 = (Book)book1.Clone();    // Clone methdou bir object döner , o yüzden Book'a dönüşüm yaptık.
+            book2.Title = "Sefiller";
+            book2.Id = 2;
+
+            Console.WriteLine(book1.Id + " " + book1.Title);
+            Console.WriteLine(book2.Id + " " + book2.Title);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
